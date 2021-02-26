ca9fc7a335f76accdb09342c40ae80fe38552b9b
Tory White <tory.m.white@gmail.com>
2021-02-26 11:52:58 -0700

    undo script move

--- web/app/themes/classical/resources/views/forms/apex-cc.blade.php
+++ web/app/themes/classical/resources/views/forms/apex-cc.blade.php
@@ -1,3 +1,1 @@
-<script> var _ctct_m = "fb465dbb6673f3c93fad93d674936817"; </script>
-<script id="signupScript" src="//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js" async defer></script>
 <div class="ctct-inline-form" data-form-id="34591a7c-6f71-4e07-8879-96b5b0df3f88"></div>
