3aa6414d564f91c933f8e1ac37f141ad1fef66fc
Tory White <tory.m.white@gmail.com>
2021-04-19 09:29:46 -0700

    remove account func until woocommerce is installed

--- web/app/themes/classical/resources/views/partials/entry-meta.blade.php
+++ web/app/themes/classical/resources/views/partials/entry-meta.blade.php
@@ -1,23 +1,23 @@
-@if(!is_account_page())
-  @if(get_field('hide_author') !== true)
-    @group('custom_author_entry')
-      @set($cname, get_sub_field('custom_name'))
-      @set($cimage, get_sub_field('custom_name'))
-    @endgroup
 
-    <p class="byline author vcard">
-      <span>{{ __('By', 'sage') }}</span>
-        @if(get_field('author'))
-          @title( get_field('author'))
-        @elseif(isset($cname) && !empty($cname))
-          {{$cname}}
-        @else
-          @author()
-        @endif
-      |
-      <time class="updated" datetime="{{ get_post_time('c', true) }}">
-      {{ get_the_date() }}
-    </time>  
-    </p>
-  @endif
+@if(get_field('hide_author') !== true)
+  @group('custom_author_entry')
+    @set($cname, get_sub_field('custom_name'))
+    @set($cimage, get_sub_field('custom_name'))
+  @endgroup
+
+  <p class="byline author vcard">
+    <span>{{ __('By', 'sage') }}</span>
+      @if(get_field('author'))
+        @title( get_field('author'))
+      @elseif(isset($cname) && !empty($cname))
+        {{$cname}}
+      @else
+        @author()
+      @endif
+    |
+    <time class="updated" datetime="{{ get_post_time('c', true) }}">
+    {{ get_the_date() }}
+  </time>  
+  </p>
 @endif
+
