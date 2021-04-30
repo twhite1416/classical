6fd0c4efc19c7404a89dde799168777cfc9b867d
Tory White <tory.m.white@gmail.com>
2021-04-29 21:55:49 -0700

    adds content field for leadership roles page

--- web/app/themes/classical/resources/views/leadership-roles.blade.php
+++ web/app/themes/classical/resources/views/leadership-roles.blade.php
@@ -13,13 +13,9 @@
             <div class="row">
               <div class="col-lg-5">
                 <h1 class="entry-title">
-                  Leadership Roles
+                  @title
                 </h1>
-                <p>Welcome to the Institute for Classical Education Job Board. In supporting the growth of classical education across the US, we are hosting this resource for the leaders and aspiring leaders of our community to connect.</p>
-                
-                <p>Please select a state, choose a job category or search by key word to find current classical education positions.</p>
-
-                <p>If you are interested in posting a job to this board select the <a href="/post-a-job">Post a Job</a> button below and follow the instructions.</p>
+                @php(the_content())
               </div>
               <div class="col-lg-7">
                 <div id="vmap" style="width: 100%; height: 400px;"></div>
